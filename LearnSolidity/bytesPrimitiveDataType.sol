5e97ca1a675cf32a6521b7a715aae649095b3aa9
Blocktical <68097949+blocktical@users.noreply.github.com>
2022-02-20 12:46:17 +0530

    Create bytesPrimitiveDataType.sol

---
+++ LearnSolidity/bytesPrimitiveDataType.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// the bytes represents a dynamic array of bytes
+contract bytesPDT {
+  bytes1 a = 0xab;
+  bytes1 b = 0x10;
+  bytes32 c = 0x17b6;
+  
+}
