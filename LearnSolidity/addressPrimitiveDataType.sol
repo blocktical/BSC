b1626d314387e6ae1f2612dd7eceac64eb53ae78
Blocktical <68097949+blocktical@users.noreply.github.com>
2022-02-20 11:51:45 +0530

    Create addressPrimitiveDataType.sol

---
+++ LearnSolidity/addressPrimitiveDataType.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract addressPDT {
+  // to store the initial address or address zero
+  address public addr = address(0);
+  
+  // any other address
+  address public addrBlocktical = 0x4ce1324cF0b701CF90E38b0515cA06C9cf33C444;
+}
