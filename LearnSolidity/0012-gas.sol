2c796740dae15992a9022b69d1f001e5bc6f0e02
Blocktical <68097949+blocktical@users.noreply.github.com>
2022-02-24 12:40:45 +0530

    Create 0012-gas.sol

---
+++ LearnSolidity/0012-gas.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract gas {
+  function burnAllGas() public {
+    string follow;
+    while ( true ) {
+      follow = "Blocktical";
+    }
+  }
+}
