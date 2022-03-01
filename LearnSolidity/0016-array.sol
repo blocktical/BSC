69552dd5257dfc3394e2a328c4cb0978441cf2c5
Blocktical <68097949+blocktical@users.noreply.github.com>
2022-03-01 21:48:40 +0530

    Update 0016-array.sol

--- LearnSolidity/0016-array.sol
+++ LearnSolidity/0016-array.sol
@@ -3,4 +3,35 @@ pragma solidity ^0.8.10;
 
 contract Array {
   uint[] public arr;
+  
+  // return index of an array
+  function getArrayIndex(uint i) public view returns (uint) {
+    return arr[i];
+  }
+  
+  // return entire array
+  function getArray() public view returns (uint[] memory) {
+    return arr;
+  }
+  
+  // get length of an array
+  function getLength() public view returns (uint) {
+    return arr.length;
+  }
+  
+  // push function OR append to an array
+  function push(uint i) public {
+    arr.push(i);
+  }
+  
+  // pop function OR remove from end of array
+  function pop() public {
+    arr.pop();
+  }
+  
+  // delete item from an array from an index
+  function delete(uint i) public {
+    delete arr[i];
+  }
+  
 }
