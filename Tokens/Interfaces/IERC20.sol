c139d3151ee2b3f77514d1cfd5edec9439042b11
TANUJ SHARMA <68097949+TANUJSH10101@users.noreply.github.com>
2022-12-17 16:32:56 +0530

    Token IERC20

--- Tokens/Interfaces/IERC20.sol
+++ Tokens/Interfaces/IERC20.sol
@@ -11,4 +11,6 @@ interface IERC20 {
     function decimal() external view returns (uint8);
     function totalSupply() external view returns (uint256);
     function balanceOf(address _owner) external view returns (uint256);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 }
